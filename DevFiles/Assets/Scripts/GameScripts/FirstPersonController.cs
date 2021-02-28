ad837ef660eb312017d2ef06873f08b2f44be0e6
GenGames <48166284+GenGames@users.noreply.github.com>
2021-02-28 11:08:56 -0600

    Test Update

---
+++ DevFiles/Assets/Scripts/GameScripts/FirstPersonController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    #region Singleton
+
+    public static FirstPersonController instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    Camera mainCam;
+    public Vector2 xyRotationSpeed;
+    public float speed;
+
+    private void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0 && Input.GetAxis("Vertical") != 0)
+        {
+            MoveCharacter();
+        }
+
+        if (Input.GetAxis("Mouse X") != 0 && Input.GetAxis("Mouse Y") != 0)
+        {
+            RotateCharacter();
+        }
+    }
+
+    public void RotateCharacter()
+    {
+        
+    }
+
+    public void MoveCharacter()
+    {
+        float XMovement = Input.GetAxis("Horizontal");
+        float ZMovement = Input.GetAxis("Vertical");
+        Vector3 movementDirection = new Vector3(Input.GetAxis())
+    }
+
+}
