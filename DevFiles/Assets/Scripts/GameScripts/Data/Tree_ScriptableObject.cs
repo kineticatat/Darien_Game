ae2213d394ab38b2bcfc1c347f40a6a6eabdc332
GenGames <48166284+GenGames@users.noreply.github.com>
2021-03-13 14:44:16 -0600

    EthanOverview

---
+++ DevFiles/Assets/Scripts/GameScripts/Data/Tree_ScriptableObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Tree", menuName = "Narrative/New Tree")]
+public class Tree_ScriptableObject : ScriptableObject
+{
+    public new string name;
+    [TextArea(15, 20)]
+    public string Description;
+}
---
+++ DevFiles/Assets/Scripts/GameScripts/TreeData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TreeData : MonoBehaviour
+{
+    public Tree_ScriptableObject treeType;
+
+    public Text[] titleDesc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateUI();
+    }
+
+    public void UpdateUI()
+    {
+        titleDesc[0].text = treeType.name;
+        titleDesc[1].text = treeType.Description;
+    }
+}
