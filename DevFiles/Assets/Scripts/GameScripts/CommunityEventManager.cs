9815190c630a2154385fc623c8a66d44571a0a93
GenGames <48166284+GenGames@users.noreply.github.com>
2021-04-11 18:43:01 -0500

    Script-half updates

---
+++ DevFiles/Assets/Scripts/GameScripts/CommunityEventManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommunityEventManager : MonoBehaviour
+{
+    private Queue<string> dialogue;
+    private CommunityEvent_ScriptableObject currentEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogue = new Queue<string>();
+    }
+
+    public void TriggeDialogue(CommunityEvent_ScriptableObject communityEvent)
+    {
+
+    }
+}
