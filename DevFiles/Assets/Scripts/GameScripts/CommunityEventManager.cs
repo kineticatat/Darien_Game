d8526c4b1eb667f10569cf87895c411f9730b65b
GenGames <48166284+GenGames@users.noreply.github.com>
2021-04-20 20:15:45 -0500

    CommunityEventsFinishingTouches

--- DevFiles/Assets/Scripts/GameScripts/CommunityEventManager.cs
+++ DevFiles/Assets/Scripts/GameScripts/CommunityEventManager.cs
@@ -1,20 +1,118 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CommunityEventManager : MonoBehaviour
 {
-    private Queue<string> dialogue;
-    private CommunityEvent_ScriptableObject currentEvent;
+    private Queue<DialogueLine> dialogues;
+    private DialogueLine currentDialogue;
+    private Queue<string> currentSentences;
+    private string speakerName;
 
-    // Start is called before the first frame update
+    public CommunityEvent_ScriptableObject currentEvent;
+
+    public CommunityEvent_ScriptableObject startingEvent;
+
+    public GameObject communityEventUI;
+    public Image backgroundImage;
+    public Text speakerText;
+    public Text dialogueText;
+
     void Start()
     {
-        dialogue = new Queue<string>();
+        dialogues = new Queue<DialogueLine>();
+        TriggerDialogue(startingEvent);
     }
 
-    public void TriggeDialogue(CommunityEvent_ScriptableObject communityEvent)
+    public void TriggerDialogue(CommunityEvent_ScriptableObject communityEvent)
     {
 
+        if (communityEvent.doesStopOtherAudio)
+        {
+            AudioManager.instance.StopAllAudio();
+        }
+        foreach (string item in communityEvent.ambientAudioToPlay)
+        {
+            AudioManager.instance.Play(item);
+        }
+
+        currentEvent = communityEvent;
+
+        dialogues = new Queue<DialogueLine>();
+
+        backgroundImage.sprite = communityEvent.backgroundImage;
+
+        foreach (DialogueLine dialogueLine in currentEvent.dialogueLines)
+        {
+            dialogues.Enqueue(dialogueLine);
+        }
+
+        FirstPersonController.instance.canMove = false;
+
+        StartNextDialogueLine();
+    }
+
+    public void StartNextDialogueLine()
+    {
+        // check and return end if this is the last set
+        if (dialogues.Count == 0) 
+        {
+            EndDialogue();
+            return;
+        }
+        currentSentences = new Queue<string>();
+                
+        currentDialogue = dialogues.Dequeue();
+
+        speakerName = currentDialogue.speakerName;
+        
+        foreach (string sentence in currentDialogue.sentences)
+        {
+            currentSentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+
+        // check and start next line if no more sentences
+        if (currentSentences.Count == 0)
+        {
+            StartNextDialogueLine();
+            return;
+        }
+
+        speakerText.text = currentDialogue.speakerName;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(currentSentences.Dequeue()));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char character in sentence.ToCharArray())
+        {
+            dialogueText.text += character;
+            yield return null;
+        }
+    }
+
+    public void EndDialogue()
+    {
+        if (currentEvent.nextDialogue != null)
+        {
+            TriggerDialogue(currentEvent.nextDialogue);
+            return;
+        }
+        if (currentEvent.locationToTravelTo != "") 
+        {
+            GameManager.instance.GoToSceneName(currentEvent.locationToTravelTo);
+            return;
+        }
+        communityEventUI.SetActive(false);
+        FirstPersonController.instance.canMove = true;
     }
 }
--- DevFiles/Assets/Scripts/GameScripts/Data/CommunityEvent_ScriptableObject.cs
+++ DevFiles/Assets/Scripts/GameScripts/Data/CommunityEvent_ScriptableObject.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Event", menuName = "Narrative/New Event")]
-public class CommunityEvent_ScriptableObject
+public class CommunityEvent_ScriptableObject : ScriptableObject
 {
-    public string name;
-    [TextArea(10,20)]
-    public string[] sentences;
-    public CommunityEvent_ScriptableObject nextDialogue;
+    [Header("Required Items")]
+    public DialogueLine[] dialogueLines;
     public Sprite backgroundImage;
+
+    [Header("Optional Items")]
+    public string eventName;
+    public CommunityEvent_ScriptableObject nextDialogue;
+    public string locationToTravelTo;
+    public bool doesStopOtherAudio;
+    public string[] ambientAudioToPlay;
+}
+
+[System.Serializable]
+public class DialogueLine
+{
+    public string speakerName;
+    [TextArea(5, 10)]
+    public string[] sentences;
 }
