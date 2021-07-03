7f6f7aa319e3be3b3ef4373cec4b37de779d5893
Bestguy2332 <54325478+Bestguy2332@users.noreply.github.com>
2021-07-03 16:28:11 -0500

    build

---
+++ DevFiles/Assets/External_Packages/3DGamekit/Scripts/Game/UI/NotebookButtons.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotebookButtons : MonoBehaviour
+{
+    public GameObject currentLocation;
+    public GameObject introFindables;
+    public GameObject artifactsPage;
+    public GameObject FaunaPage;
+    public GameObject floraPages;
+    public Image background;
+    public Sprite currentLocationBack;
+    public Sprite normalBackgorund;
+
+    void Start()
+    {
+        
+    
+    }
+    public void ToCurrentLocationPage()
+    {
+        currentLocation.SetActive(true);
+        introFindables.SetActive(false);
+        artifactsPage.SetActive(false);
+        FaunaPage.SetActive(false);
+        floraPages.SetActive(false);
+      
+    } 
+    
+    public void ToIntroFindablePage()
+    {
+        currentLocation.SetActive(false);
+        introFindables.SetActive(true);
+        artifactsPage.SetActive(false);
+        FaunaPage.SetActive(false);
+        floraPages.SetActive(false);
+    }  
+    public void ToArtifactsPage()
+    {
+        currentLocation.SetActive(false);
+        introFindables.SetActive(false);
+        artifactsPage.SetActive(true);
+        FaunaPage.SetActive(false);
+        floraPages.SetActive(false);
+    } 
+    public void ToFaunaPage()
+    {
+        currentLocation.SetActive(false);
+        introFindables.SetActive(false);
+        artifactsPage.SetActive(false);
+        FaunaPage.SetActive(true);
+        floraPages.SetActive(false);
+    } 
+    public void ToFloraPage()
+    {
+        currentLocation.SetActive(false);
+        introFindables.SetActive(false);
+        artifactsPage.SetActive(false);
+        FaunaPage.SetActive(false);
+        floraPages.SetActive(true);
+    }
+}
