77825c1736c403b92f9d8bddb4c5a0c61b185347
Bestguy2332 <54325478+Bestguy2332@users.noreply.github.com>
2021-07-25 12:57:14 -0500

    Compass and santa barbra Player update

---
+++ DevFiles/Assets/compass.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class compass : MonoBehaviour
+{
+    public GameObject playerCompass;
+    public GameObject player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerCompass.transform.eulerAngles = new Vector3(0, 0, player.transform.eulerAngles.y);
+    }
+}
